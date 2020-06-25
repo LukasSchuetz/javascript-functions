4cb42ec994594748d4fbd58c169a3d536df910cb
lukas.schuetz <lukas.schuetz@accenture.com>
2020-06-25 12:45:26 +0200

    Solved first part

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,15 +1,73 @@
-function seed() {}
+const { jsxSpreadAttribute } = require("jscodeshift");
 
-function same([x, y], [j, k]) {}
+function seed(a, b, c) {
+  return [...arguments];
+}
+
+function same([x, y], [j, k]) {
+  if ((x === j) && (y === k)) {
+    return true;
+  }
+}
 
 // The game state to search for `cell` is passed as the `this` value of the function.
-function contains(cell) {}
+function contains(cell) {
+  for (let c of this) {
+    if (same(c, cell)) return true;
+  }
+  return false;
+}
 
-const printCell = (cell, state) => {};
+const printCell = (cell, state) => {
+  if (contains.call(state, cell)) {
+    return '\u25A3';
+  } else {
+    return '\u25A2';
+  }
+};
 
-const corners = (state = []) => {};
+const corners = (state = []) => {
+  if (state.length === 0) {
+    return { topRight: [0, 0], bottomLeft: [0, 0] }
+  } else {
+    let xmin = state[0][0];
+    let xmax = state[0][0];
+    let ymin = state[0][1];
+    let ymax = state[0][1];
+    
+    for (let cell of state) {
+      if (cell[0] < xmin) {
+        xmin = cell[0];
+      }
+      if (cell[0] > xmax) {
+        xmax = cell[0];
+      }
+      if (cell[1] < ymin) {
+        ymin = cell[1];
+      }
+      if (cell[1] > ymax) {
+        ymax = cell[1];
+      }
+    }
+    return { topRight: [xmax, ymax], bottomLeft: [xmin, ymin] };
+  }
+};
 
-const printCells = (state) => {};
+const printCells = (state) => {
+  const { topRight, bottomLeft } = corners(state);
+  let xmin = bottomLeft[0];
+  let xmax = topRight[0];
+  let ymin = bottomLeft[1];
+  let ymax = topRight[1];
+  result = '';
+  for (let y = ymax; y >= ymin; y--) {
+    for (let x = xmin; x <= xmax; x++) {
+      result += `${printCell([x,y], state)} `;
+    }
+    result += '\n';
+  }
+  return result;
+};
 
 const getNeighborsOf = ([x, y]) => {};
 
